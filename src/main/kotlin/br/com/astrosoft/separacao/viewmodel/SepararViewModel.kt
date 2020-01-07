c18b610d21bd9f5c2c8d1eb7da9f719083c9bf77
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2020-01-07 16:47:46 -0300

    Correção de bug na separação

--- src/main/kotlin/br/com/astrosoft/separacao/viewmodel/SepararViewModel.kt
+++ src/main/kotlin/br/com/astrosoft/separacao/viewmodel/SepararViewModel.kt
@@ -20,19 +20,20 @@ class SepararViewModel(view: ISepararView): ViewModel<ISepararView>(view) {
       throw EViewModelError("Não há nenhum produto selecionado")
     else
       produtosSelecionados.forEach {produto ->
-        saci.atualizarQuantidade(ordno, proximoNumero, produto.prdnoSaci, produto.grade, produto.qttyEdit.toDouble())
+        saci.atualizarQuantidade(ordno, proximoNumero, produto.prdnoSaci, produto.grade,
+                                 produto.localizacao, produto.qttyEdit.toDouble())
       }
     print(proximoNumero)
     view.showInformation("Foi gerado o pedido número $proximoNumero")
     view.updateGrid()
   }
   
-  fun imprimir()= exec {
+  fun imprimir() = exec {
     val pedido = view.pedido ?: throw EViewModelError("Pedido inválido")
     print(pedido.ordno)
   }
   
-  private fun print(ordno : Int){
+  private fun print(ordno: Int) {
     Ssh("172.20.47.1", "ivaney", "ivaney").shell {
       execCommand("/u/saci/shells/printRessuprimento.sh $ordno")
     }
