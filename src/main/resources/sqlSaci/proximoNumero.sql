e365544c249e5e50dc81bacb4d40b3c62bd3f7b7
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2020-01-09 08:29:33 -0300

    hotfix - Corrige o erro de numeração na duplicação quando o número do pedido é um milhar (ex. 5000, 3000, 2000)

--- src/main/resources/sqlSaci/proximoNumero.sql
+++ src/main/resources/sqlSaci/proximoNumero.sql
@@ -1,4 +1,4 @@
 SELECT (MAX(no) + 1) AS proximo
 FROM ords
 WHERE storeno = :storeno
-  AND no BETWEEN (:destino * 1000 + 1) AND (:destino * 1000 + 999)
+  AND no BETWEEN (:destino * 1000) AND (:destino * 1000 + 999)
