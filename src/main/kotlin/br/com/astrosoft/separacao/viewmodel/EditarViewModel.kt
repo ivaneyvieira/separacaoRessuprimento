a57041e516a9ec79def2513921eb9d97f115850e
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2020-01-14 10:22:00 -0300

    Conserto do bug

--- src/main/kotlin/br/com/astrosoft/separacao/viewmodel/EditarViewModel.kt
+++ src/main/kotlin/br/com/astrosoft/separacao/viewmodel/EditarViewModel.kt
@@ -1,5 +1,6 @@
 package br.com.astrosoft.separacao.viewmodel
 
+import br.com.astrosoft.framework.viewmodel.EViewModelError
 import br.com.astrosoft.framework.viewmodel.IView
 import br.com.astrosoft.framework.viewmodel.ViewModel
 import br.com.astrosoft.separacao.model.beans.Pedido
@@ -9,8 +10,14 @@ import br.com.astrosoft.separacao.model.saci
 
 class EditarViewModel(view: IEditarView): ViewModel<IEditarView>(view) {
   fun processar() = exec {
+    val pedido = view.pedido ?: throw EViewModelError("Nenum pedido selecionado")
     view.produtos.forEach {produto ->
-      //saci.retornaSaldo(produto)
+      saci.retornaSaldo(ordnoMae = pedido.ordnoMae,
+                        ordno = pedido.ordno,
+                        codigo = produto.codigo,
+                        grade = produto.grade,
+                        diferenca = produto.diferenca,
+                        localizacao = produto.localizacao)
     }
   }
   
