754e26d9ed0793b9602e10033d9a889f77aa7647
Ivaney Vieira <ivaneyvieira@gmail.com>
2020-11-12 08:54:36 -0300

    Ajuste provisorio do campo auxStr - Localizacao dos pedidos importado de outras lojas

--- src/main/resources/sqlSaci/listaProdutos.sql
+++ src/main/resources/sqlSaci/listaProdutos.sql
@@ -1,15 +1,20 @@
-SELECT O.prdno, O.grade, IFNULL(TRIM(MID(P.name, 1, 37)), '') AS descricao, P.mfno AS fornecedor,
-       IFNULL(LPAD(P.clno, 6, '0'), '') AS centrodelucro,
-       IFNULL(L.localizacao, O.auxStr) AS localizacao, P.typeno AS tipo,
-       IFNULL(ROUND(O.auxLong3 / 1000, 2), 0.00) AS qttyOriginal,
-       IFNULL(ROUND(O.qtty, 2), 0.00) AS qtty, IFNULL(ROUND(S.qtty_varejo / 1000, 2), 0.00) AS saldo
+SELECT O.prdno,
+       O.grade,
+       IFNULL(TRIM(MID(P.name, 1, 37)), '')                           AS descricao,
+       P.mfno                                                         AS fornecedor,
+       IFNULL(LPAD(P.clno, 6, '0'), '')                               AS centrodelucro,
+       IFNULL(L.localizacao, IF(LENGTH(O.auxStrO) > 8, '', O.auxStr)) AS localizacao,
+       P.typeno                                                       AS tipo,
+       IFNULL(ROUND(O.auxLong3 / 1000, 2), 0.00)                      AS qttyOriginal,
+       IFNULL(ROUND(O.qtty, 2), 0.00)                                 AS qtty,
+       IFNULL(ROUND(S.qtty_varejo / 1000, 2), 0.00)                   AS saldo
 FROM sqldados.oprd           AS O
   INNER JOIN sqldados.prd    AS P
-               ON (O.prdno = P.no)
+	       ON (O.prdno = P.no)
   LEFT JOIN  sqldados.prdloc AS L
-               ON (O.prdno = L.prdno AND O.grade = L.grade AND O.auxStr = '')
+	       ON (O.prdno = L.prdno AND O.grade = L.grade AND LENGTH(O.auxStrO) <= 8)
   LEFT JOIN  sqldados.stk    AS S
-               ON (O.prdno = S.prdno AND O.grade = S.grade AND S.storeno = 4)
-WHERE O.storeno = :storeno AND
-      O.ordno = :ordno
+	       ON (O.prdno = S.prdno AND O.grade = S.grade AND S.storeno = 4)
+WHERE O.storeno = :storeno
+  AND O.ordno = :ordno
 GROUP BY prdno, grade, IFNULL(L.localizacao, O.auxStr)
