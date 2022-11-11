b3c73e9a5bc06b2aba9bcd9486b4131254f48c28
Ivaney Sales <ivaneyvieira@gmail.com>
2022-11-11 05:48:53 -0300

    Resistção dos pedidos pendentes entreo 10000 e 99999

--- src/main/resources/sqlSaci/listaPedidosPendente.sql
+++ src/main/resources/sqlSaci/listaPedidosPendente.sql
@@ -11,10 +11,9 @@ FROM sqldados.ordsPendente            O
   INNER JOIN sqldados.oprdPendente AS P
 	       ON O.storeno = P.storeno AND O.no = P.ordno AND P.date = O.date AND
 		  P.prdno <> LPAD('19', 16, ' ')
-WHERE (O.no IN (54) OR no BETWEEN 10000 AND 99999 OR NO BETWEEN 100000000 AND 999999999)
+WHERE (O.no BETWEEN 10000 AND 99999)
   AND MID(no, 1, 1) * 1 IN (2, 3, 4, 5)
   AND O.storeno = :storeno;
-
 
 SELECT storeno,
        ordno,
@@ -27,7 +26,7 @@ FROM T_ORDS                                                              AS P
 		    O.date AS data,
 		    prdno,
 		    grade
-	     FROM sqldados.ords          AS O
+	     FROM sqldados.ords         AS O
 	       INNER JOIN sqldados.oprd AS E
 			    ON O.storeno = E.storeno AND O.no = E.ordno) AS D
 	      USING (storeno, ordno, data, prdno, grade)
