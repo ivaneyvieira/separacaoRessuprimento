e962c485a3f5c786d75e297982bfd3b75f2860c4
Ivaney Sales <ivaneyvieira@gmail.com>
2022-11-14 14:33:52 -0300

    Ajuste no filtro do painel pendente

--- src/main/resources/sqlSaci/removerPedido.sql
+++ src/main/resources/sqlSaci/removerPedido.sql
@@ -1,10 +1,11 @@
-REPLACE INTO ordsPendente(no, date, vendno, discount, amt, package, custo_fin, others, eord_ordno,
-			  dataFaturamento, invno, freightAmt, auxLong1, auxLong2, amtOrigem,
-			  dataEntrega, discountOrig, l1, l2, l3, l4, m1, m2, m3, m4, deliv, storeno,
-			  carrno, empno, prazo, eord_storeno, delivOriginal, bits, bits2, bits3,
-			  padbyte, indxno, repno, auxShort1, auxShort2, noofinst, status, s1, s2,
-			  s3, s4, frete, remarks, ordnoFromVend, remarksInv, remarksRcv, remarksOrd,
-			  auxChar, c1, c2, c3, c4)
+INSERT IGNORE INTO ordsPendente(no, date, vendno, discount, amt, package, custo_fin, others,
+				eord_ordno, dataFaturamento, invno, freightAmt, auxLong1, auxLong2,
+				amtOrigem,
+				dataEntrega, discountOrig, l1, l2, l3, l4, m1, m2, m3, m4, deliv,
+				storeno, carrno, empno, prazo, eord_storeno, delivOriginal, bits,
+				bits2, bits3, padbyte, indxno, repno, auxShort1, auxShort2,
+				noofinst, status, s1, s2, s3, s4, frete, remarks, ordnoFromVend,
+				remarksInv, remarksRcv, remarksOrd, auxChar, c1, c2, c3, c4)
 SELECT no,
        date,
        vendno,
@@ -64,14 +65,15 @@ SELECT no,
        c4
 FROM ords o
 WHERE storeno = :storeno
-  AND no BETWEEN :numeroI AND :numeroF;
+  AND no BETWEEN :ordno AND :ordno;
 
-REPLACE INTO oprdPendente(date, ordno, mult, ipi, freight, icms, auxLong1, auxLong2, auxMy1,
-			  auxMy2, icmsSubst, auxLong3, auxLong4, auxMy3, auxMy4, qtty, qtty_src,
-			  qtty_xfr, cost, qttyRcv, qttyCancel, qttyVendaMes, qttyVendaMesAnt,
-			  qttyVendaMedia, qttyPendente, stkDisponivel, qttyAbc, storeno, seqno,
-			  status, bits, bits2, auxShort1, auxShort2, auxShort3, auxShort4, prdno,
-			  grade, remarks, padbyte, gradeFechada, obs, auxStr)
+INSERT IGNORE INTO oprdPendente(date, ordno, mult, ipi, freight, icms, auxLong1, auxLong2, auxMy1,
+				auxMy2, icmsSubst, auxLong3, auxLong4, auxMy3, auxMy4, qtty,
+				qtty_src, qtty_xfr, cost, qttyRcv, qttyCancel, qttyVendaMes,
+				qttyVendaMesAnt, qttyVendaMedia, qttyPendente, stkDisponivel,
+				qttyAbc, storeno, seqno, status, bits, bits2, auxShort1, auxShort2,
+				auxShort3, auxShort4, prdno, grade, remarks, padbyte, gradeFechada,
+				obs, auxStr)
 SELECT O.date,
        ordno,
        mult,
@@ -119,7 +121,7 @@ FROM ords         AS O
   INNER JOIN oprd AS P
 	       ON O.no = P.ordno AND P.storeno = O.storeno
 WHERE O.storeno = :storeno
-  AND O.no BETWEEN :numeroI AND :numeroF;
+  AND O.no BETWEEN :ordno AND :ordno;
 
 DELETE
 FROM ords
